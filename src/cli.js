6120ba98d3c2264f2da58a6da36050cd478dec98
Maryna Redkina <marina.kliuieva@gmail.com>
2021-12-08 09:55:30 -0500

    step 3 add readlinesync add function

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from "readline-sync";
+const greeting = () => { 
+    const name = readlineSync.question('May I have your name? ');
+console.log('Hi ' + name + '!');
+}
+
+export default greeting;
