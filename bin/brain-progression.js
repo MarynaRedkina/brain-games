612189f5dc25f90954e20af5d246655eba42214a
Maryna Redkina <marina.kliuieva@gmail.com>
2021-12-24 16:27:54 -0500

    add brain-progression.js

---
+++ bin/brain-progression.js
@@ -0,0 +1,53 @@
+import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+
+const name = greeting();
+
+console.log('What number is missing in the progression?');
+
+const getBrainProgression = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+    const number = getRandomNumber(0, 100);
+    const increment = getRandomNumber(0, 5);
+    const progressionLength = 10;
+    const progression = [number];
+
+    const getProgression = () => {
+      for (let j = 0; j < progressionLength; j += 1) {
+        progression.push(progression[i] + increment);
+      }
+      return 0;
+    };
+    getProgression();
+
+    const randomIndex = getRandomNumber(0, 9);
+    //  console.log(randomIndex);
+    const correctAnswer = progression[randomIndex];
+    //  console.log(correctAnswer);
+    const substitutedNumber = '...';
+
+    progression[randomIndex] = substitutedNumber;
+
+    // console.log(progression);
+
+    const string = progression.join(' ');
+
+    console.log(`Question: ${string}`);
+
+    const guess = parseInt(readlineSync.question('Your answer: '), 10);
+
+    if (guess === correctAnswer) {
+      console.log('Correct!');
+    } if (guess !== correctAnswer) {
+      console.log(`${guess} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+      console.log(`Let's try again, ${name}`);
+      return 0;
+    }
+  }
+  return 0;
+};
+getBrainProgression();
