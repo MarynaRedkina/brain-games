a89cff02f0939689afb185c16e515864e161a30d
Maryna Redkina <marina.kliuieva@gmail.com>
2022-01-14 11:50:36 -0500

    add corrected brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 import greeting from '../src/cli.js';
+import getRandomNumber from '../src/utils.js';
 
 console.log('Welcome to the Brain Games!');
 
@@ -7,25 +8,45 @@ const name = greeting();
 
 console.log('What is the result of the expression?');
 
+// const b = ['+', '-', '*'];
+// const operator = possibleOperators[getRandomNumber(0, possibleOperators.length-1)];
+
+// const firstNumber = getRandomNumber(0, 10);
+// const secondNumber = getRandomNumber(0, 10);
+
+const getCorrectAnswer = (a, b, c) => {
+  switch (b) {
+    case '+':
+      return a + c;
+    case '-':
+      return a - c;
+    case '*':
+      return a * c;
+    default:
+      console.log('Error');
+  }
+  return 0;
+};
+
 const brainCalc = () => {
   for (let i = 0; i < 3; i += 1) {
     // const randomNumber = Math.floor(Math.random() * 100);
-    const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    // const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;hh
     // console.log(getRandomNumber(0, 100));
 
-    const possibleOperators = '+-*';
-    const getOperator = (str) => str[getRandomNumber(0, 2)];
+    const possibleOperators = ['+', '-', '*'];
+    // const getOperator = (str) => str[getRandomNumber(0, 2)];
     // console.log(getOperator(operator));
-    const operator = getOperator(possibleOperators);
+    const operator = possibleOperators[getRandomNumber(0, possibleOperators.length - 1)];
 
     const firstNumber = getRandomNumber(0, 10);
     const secondNumber = getRandomNumber(0, 10);
 
-    const makeQuestion = () => `Question: ${firstNumber} ${operator} ${secondNumber}`;
+    // const makeQuestion = () => `Question: ${firstNumber} ${operator} ${secondNumber}`;
 
-    console.log(makeQuestion(firstNumber, operator, secondNumber));
+    console.log(`Question: ${firstNumber} ${operator} ${secondNumber}`);
 
-    const getCorrectAnswer = () => {
+    /* const getCorrectAnswer = () => {
       switch (operator) {
         case '+':
           return firstNumber + secondNumber;
@@ -37,9 +58,9 @@ const brainCalc = () => {
           console.log('Error');
       }
       return 0;
-    };
+    }; */
 
-    const correctAnswer = getCorrectAnswer();
+    const correctAnswer = getCorrectAnswer(firstNumber, operator, secondNumber);
 
     const guess = parseInt(readlineSync.question('Your answer: '), 10);
 
@@ -50,8 +71,8 @@ const brainCalc = () => {
       console.log(`Let's try again, ${name}`);
       return 0;
     }
-    console.log(`Congratulations, ${name}`);
   }
+  console.log(`Congratulations, ${name}`);
   return 0;
 };
 
